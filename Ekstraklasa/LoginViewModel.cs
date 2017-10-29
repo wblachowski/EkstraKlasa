65de25bb2472f90522f744bfd2fb8ee009ab0c2a
wblachowski <wojciech@blachowski.pl>
2017-10-29 13:48:15 +0100

    LoginViewModel

---
+++ Ekstraklasa/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ekstraklasa
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = null;
+        public LoginViewModel()
+        {
+        }
+
+        private ICommand _loginCommand;
+        public ICommand loginCommand
+        {
+            get
+            {
+                if (_loginCommand == null)
+                {
+                    _loginCommand = new RelayCommand(param => this.Login());
+                }
+                return _loginCommand;
+            }
+        }
+        private string _Username = null;
+        public string Username
+        {
+            get
+            {
+                return _Username;
+            }
+            set
+            {
+                if (value != _Username)
+                {
+                    _Username = value;
+                    OnPropertyChanged("Username");
+                }
+            }
+        }
+        private string _Password = null;
+        public string Password
+        {
+            get
+            {
+                return _Password;
+            }
+            set
+            {
+                _Password = null;
+                OnPropertyChanged("Password");
+            }
+
+        }
+
+        private void Login()
+        {
+            System.Diagnostics.Debug.WriteLine(this._Username);
+
+        }
+        virtual protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
--- Ekstraklasa/LoginWindow.xaml.cs
+++ Ekstraklasa/LoginWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace Ekstraklasa
         public LoginWindow()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
         }
     }
 }
