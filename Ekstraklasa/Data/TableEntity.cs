db75fe1116441ba7a994d4cc8baebce7f4196c21
wblachowski <wojciech@blachowski.pl>
2017-11-08 17:45:54 +0100

    Table related classes

---
+++ Ekstraklasa/Data/TableEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekstraklasa
+{
+    public class TableEntity
+    {
+        public TableEntity(string Nr,string Name,string Matches,string Wins,string Ties,string Loses, string Goals,string Points)
+        {
+            this.Nr = Nr;
+            this.Name = Name;
+            this.Matches = Matches;
+            this.Wins = Wins;
+            this.Ties = Ties;
+            this.Loses = Loses;
+            this.Goals = Goals;
+            this.Points = Points;
+        }
+
+        public string Nr;
+        public string Name;
+        public string Matches;
+        public string Wins;
+        public string Ties;
+        public string Loses;
+        public string Goals;
+        public string Points;
+    }
+}
---
+++ Ekstraklasa/ViewModels/TableViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekstraklasa
+{
+    class TableViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = null;
+
+        private ObservableCollection<TableEntity> _TableEntities;
+        public ObservableCollection<TableEntity> TableEntities
+        {
+            get
+            {
+                return _TableEntities;
+            }
+            set
+            {
+                if (!TableEntities.Equals(value))
+                {
+                    TableEntities = value;
+                    OnPropertyChanged("TableEntities");
+                }
+            }
+        }
+
+        virtual protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
