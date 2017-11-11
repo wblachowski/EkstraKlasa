90dd605eea0680c01539f5b3f7466daa02b2d02a
wblachowski <wojciech@blachowski.pl>
2017-11-11 13:42:23 +0100

    -Add entities -Change styles

---
+++ Ekstraklasa/Data/GoalEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekstraklasa
+{
+    public class GoalEntity
+    {
+        public GoalEntity(int Minute, string Firstname, string Lastname, bool HostGoal)
+        {
+            this.Minute = Minute;
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.HostGoal = HostGoal;
+        }
+
+        public int Minute;
+        public string Firstname;
+        public string Lastname;
+        public bool HostGoal;
+    }
+}
---
+++ Ekstraklasa/Data/MatchEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekstraklasa
+{
+    public class MatchEntity
+    {
+        public MatchEntity(int ID,DateTime Date, string Host, string Guest, int ScoreHost, int ScoreGuest, string Stadium, string City, string Address)
+        {
+            this.ID = ID;
+            this.Date = Date;
+            this.Host = Host;
+            this.Guest = Guest;
+            this.ScoreHost = ScoreHost;
+            this.ScoreGuest = ScoreGuest;
+            this.Stadium = Stadium;
+            this.City = City;
+            this.Address = Address;
+        }
+
+        public int ID;
+        public DateTime Date;
+        public string Host;
+        public string Guest;
+        public int ScoreHost;
+        public int ScoreGuest;
+        public string Stadium;
+        public string City;
+        public string Address;
+    }
+}
