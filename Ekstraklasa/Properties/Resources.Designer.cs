6dc203ba17edbe35543b58575ca0bd86ebafd374
wblachowski <wojciech@blachowski.pl>
2017-11-26 13:24:13 +0100

    Icon

--- Ekstraklasa/Properties/Resources.Designer.cs
+++ Ekstraklasa/Properties/Resources.Designer.cs
@@ -59,5 +59,24 @@ namespace Ekstraklasa.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        internal static string String1 {
+            get {
+                return ResourceManager.GetString("String1", resourceCulture);
+            }
+        }
     }
 }
